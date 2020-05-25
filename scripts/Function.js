ce661965215f6fdeeaa2032f4bf1244c8dba10c2
00Greenwood <marcus_greenwood@hotmail.co.uk>
2020-05-25 09:41:38 +0100

    Add some function types

--- scripts/Function.js
+++ scripts/Function.js
@@ -1,5 +1,45 @@
 class Function {
-  constructor(){
-    
+  constructor() {
+    if (this.constructor == Function) {
+      throw new Error("Abstract classes can't be instantiate.");
+    }
+  }
+
+  evaluate(x) {
+    throw new Error("Method evaluate() must be implemented.");
+  }
+}
+
+class ValueFunction extends Function {
+  constructor(value) {
+    super();
+    this.value = value;
+  }
+
+  evaluate(x) {
+    return this.value;
+  }
+}
+
+class PowerFunction extends Function {
+  constructor(power) {
+    super();
+    this.power = power;
+  }
+
+  evaluate(x) {
+    return Math.pow(x, this.power);
+  }
+}
+
+class AddFunction extends Function {
+  constructor(f1, f2) {
+    super();
+    this.f1 = f1;
+    this.f2 = f2;
+  }
+
+  evaluate(x) {
+    return this.f1.evaluate() + this.f2.evaluate();
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-var functionString = "4x+6"
+let valueFunction = new ValueFunction(6);
+let powerFunction = new PowerFunction(1);
+let addFvaunction = new AddFunction(powerFunction, valueFunction);
+let x = valueFunction.evaluate(6);
